5ca865d21223e09b0361d73b29acae906a04209a
Elan Fraga <elanfraga@gmail.com>
2019-08-15 21:01:56 -0300

    Listando repositórios

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -57,7 +57,7 @@ export const SubmitButton = styled.button.attrs(props => ({
   justify-content: center;
   align-items: center;
 
-  &[disable] {
+  &[disabled] {
     cursor: not-allowed;
     opacity: 0.6;
   }
@@ -69,4 +69,26 @@ export const SubmitButton = styled.button.attrs(props => ({
         animation: ${rotate} 2s linear infinite;
       }
     `}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #7159c1;
+      text-decoration: nome;
+    }
+  }
 `;
